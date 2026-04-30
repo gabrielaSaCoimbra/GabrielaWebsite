2cd82a0a6eaee819c892f447166af920384b8341
diogofbrito <diogobrito74@gmail.com>
2026-04-30 17:10:28 +0100

    feat: new

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,6 +1,5 @@
-import about from './about'
 import ambientItem from './ambientItem'
 import project from "./project"
 import worksOverview from "./worksOverview"
 
-export const schemaTypes = [about, ambientItem, project, worksOverview]
+export const schemaTypes = [ ambientItem, project, worksOverview]
